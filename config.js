7296892d0a097213020f7736dba5de9506e5f83d
66Shadowlord66 <666damonlewis666@protonmail.com>
2025-02-14 23:30:57 +1030

    Update config.js

--- config.js
+++ config.js
@@ -15,11 +15,11 @@ module.exports = {
   errorLog: "", 
   nodes: [
  {
-  "identifier": "INZEWORLD.COM (DE)",
-  "password": "saher.inzeworld.com",
-  "host": "lava.inzeworld.com",
-  "port": 3128,
-  "secure": false
- }
+      name: "INZEWORLD.COM (DE)",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
+      secure: false
+    },
   ]
 }
