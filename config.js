dd82c6333e48f6b2217acdc2448ec753bf4a8ad3
66Shadowlord66 <666damonlewis666@protonmail.com>
2025-02-14 22:46:51 +1030

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://Big_Smoke_DJ:Fuckingpassword1!@big-smoke-dj.0soul.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
